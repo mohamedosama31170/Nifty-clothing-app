cdc7357d54119bf87cd3714a0f62c4204f1079e1
mahmoudatta80 <atta89383@gmail.com>
2025-11-29 16:30:27 +0200

    update url

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class AppConstants {
   static const String appTitle = 'Nifty Clothing';
-  static const String initialUrl = 'https://nifty-clothing.com/';
+  static const String initialUrl = 'https://nifty-clothings.com';
   static const String splashScreenImage = 'assets/app_splash.png';
   static const Color primaryColor = Color(0xff4E035B);
 }
